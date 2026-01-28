cfbe749139d7da6f311aab84271cc73c1c39088b
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-01-29 00:13:43 +0100

    feat: add RSS feed generation and update configuration for site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://extraordinary-horse-4eec8c.netlify.app/'
+});
